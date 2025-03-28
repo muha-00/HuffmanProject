d7ad9756bce0c2f00eae4eaa7a2dedf5793078bb
Muhammad <muhammad87711@gmail.com>
2025-03-28 18:45:01 -0400

    added the frequency table and huffman code table printers

--- main.cpp
+++ main.cpp
@@ -29,5 +29,36 @@ std::map<char, int> countCharacterFrequencies(const std::string& filename) {
 
     return freqMap;
 }
+// displays character frequency count in a formatted table
+void printFrequencyTable(const std::map<char, int>& freqMap) {
+    std::cout << "\nFrequency Table:\n";
+    for(const auto& pair : freqMap) {
+        // Formats special characters for readability
+        std::string displayChar;
+        if(pair.first == ' ') displayChar = "Space";
+        else if(pair.first == '\n') displayChar = "Newline";
+        else if(pair.first == '\t') displayChar = "Tab";
+        else if(std::isprint(pair.first)) displayChar = std::string("'") + pair.first + "'";
+
+        // this aligns output in columns
+        std::cout << std::setw(8) << std::left << displayChar << ": " << pair.second << "\n";
+    }
+
+
+// this prints the  generated huffman codes with good formatting
+void printCodes(const std::map<char, std::string>& codes) {
+    std::cout << "\nHuffman Codes:\n";
+    for(const auto& pair : codes) {
+        // the same  same special character formatting as frequency table, to account for space character, new line and tab
+        std::string displayChar;
+        if(pair.first == ' ') displayChar = "Space";
+        else if(pair.first == '\n') displayChar = "Newline";
+        else if(pair.first == '\t') displayChar = "Tab";
+        else if(std::isprint(pair.first)) displayChar = std::string("'") + pair.first + "'";
+
+        std::cout << std::setw(8) << std::left << displayChar << ": " << pair.second << "\n";
+    }
+}
+
 
 
