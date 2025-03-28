54fb129799c88017b81fc9da8ffc7fb44b3cb340
Muhammad <muhammad87711@gmail.com>
2025-03-28 16:30:20 -0400

    initialized constructor, destructor,and created memory cleanup helper

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "HuffmanTree.h"
+#include <stdexcept>
+
+//constructor initialized with a null root
+HuffmanTree::HuffmanTree() : root(nullptr) {}
+
+//destructor to clean up all the nodes
+HuffmanTree::~HuffmanTree() {
+    deleteTree(root);
+}
+
+//recursive deletion method implemented for tree cleanup
+void HuffmanTree::deleteTree(HuffmanNode* node) {
+    if(!node) return;
+    deleteTree(node->left);
+    deleteTree(node->right);
+    delete node;
+}
+
